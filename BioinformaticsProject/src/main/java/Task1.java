93231e075dbc35c168206af663e990c4f3ef9672
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2016-10-22 17:51:46 +0200

    Task 1 brute forced. Uncertain whether it is right or not

--- BioinformaticsProject/src/main/java/Task1.java
+++ BioinformaticsProject/src/main/java/Task1.java
@@ -2,30 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
-/**
- * Created by oyvin on 20/10/2016.
- */
 public class Task1 {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/s_3_sequence_1M.txt"))) {
-
+            int matches = 0;
             String line = br.readLine();
             String pattern = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG";
             ArrayList<String> prefixes = new ArrayList<>();
+            Map<Integer, Integer> lengthDist = new HashMap<>();
             for (int i = 1; i <= pattern.length(); i++) {
                 String prefix = pattern.substring(0, i);
                 prefixes.add(prefix);
             }
             while (line != null) {
-                ArrayList<String> suffixes = new ArrayList<>();
+
                 for (int j = line.length() - 1; j >= 0; j--) {
                     String suffix = line.substring(j, line.length());
-                    suffixes.add(suffix);
+                    int length = line.length();
+                    for (String prefix : prefixes){
+                        if (suffix.equals(prefix)){
+                            length -= suffix.length();
+                        }
+
+                    }
+
+                    Integer n = lengthDist.get(length);
+                    n = (n == null)? 1 : ++n;
+                    lengthDist.put(length,n);
+
                 }
                 line = br.readLine();
+
             }
+            System.out.println(lengthDist);
 
         }
     }
