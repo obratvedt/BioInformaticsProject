83a65f70efe5a52091b4b0a44fff08c4039125ca
Øyvind Bratvedt <oyvbr@stud.ntnu.no>
2016-10-22 17:06:36 +0200

    Added all prefixes of the pattern and all suffixes of the sequences

--- BioinformaticsProject/src/main/java/Task1.java
+++ BioinformaticsProject/src/main/java/Task1.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 /**
@@ -11,16 +12,21 @@ public class Task1 {
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/resources/s_3_sequence_1M.txt"))) {
 
             String line = br.readLine();
-            int totalLength = 0;
             String pattern = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG";
+            ArrayList<String> prefixes = new ArrayList<>();
+            for (int i = 1; i <= pattern.length(); i++) {
+                String prefix = pattern.substring(0, i);
+                prefixes.add(prefix);
+            }
             while (line != null) {
-                if (line.substring(line.length() - 4, line.length() - 1).equals(pattern.substring(0, 3))) totalLength++;
+                ArrayList<String> suffixes = new ArrayList<>();
+                for (int j = line.length() - 1; j >= 0; j--) {
+                    String suffix = line.substring(j, line.length());
+                    suffixes.add(suffix);
+                }
                 line = br.readLine();
             }
-            System.out.println(totalLength);
+
         }
-
-
-
     }
 }
